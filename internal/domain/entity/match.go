bdb66724037a8a1bd1a983824a08f6418677dfe0
LucasRosario <lucasrosario1@hotmail.com>
2022-12-07 17:11:18 -0300

    Aula 1 iniciada , servico do time 1-08-23

---
+++ internal/domain/entity/match.go
@@ -0,0 +1,42 @@
+package entity
+
+import "time"
+
+type MatchResult struct {
+	teamAScore int
+	teamBScore int
+}
+
+func NewMatchResult(teamAScore, teamBScore int) *MatchResult {
+	return &MatchResult{
+		teamAScore: teamAScore,
+		teamBScore: teamBScore,
+	}
+}
+
+func (m *MatchResult) GetResult() string {
+
+}
+
+type Match struct {
+	ID      string
+	TeamA   *Team
+	TeamB   *Team
+	TeamAID string
+	TeamBID string
+	Date    time.Time
+	Status  string
+	Result  MatchResult
+	Actions []GameAction
+}
+
+func NewMatch(id string, teamA *Team, teamB *Team, date time.Time) *Match {
+	return &Match{
+		ID:      id,
+		TeamA:   teamA,
+		TeamB:   teamB,
+		TeamAID: teamA.ID,
+		TeamBID: teamB.ID,
+		Date:    date,
+	}
+}
